ca7d09134361616c70c7be724dcec3442ba5c715
sinitreo <sinitreo@gmail.com>
2018-01-21 04:53:35 +0500

    - added build

---
+++ build/app/nlmqjyxt.js
@@ -0,0 +1,4 @@
+/*! Built with http://stenciljs.com (es5) */
+
+App.loadStyles("app-profile","app-profile div{padding:10px}\napp-profile.hydrated{visibility:inherit}");
+App.loadComponents("nlmqjyxt",function(a,n,t,e){"use strict";var p=function(){function a(){}return a.prototype.render=function(){if(this.match&&this.match.params.name)return n("div",null,n("p",null,"Hello! My name is ",this.match.params.name,". My name was passed in through a route param!"))},a}();a["app-profile"]=p},["app-profile",[["match",1,1,1]],{}]);;
